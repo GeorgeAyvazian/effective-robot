1c144f7b5742a73fc15dbb969ab38cb2a88c9395
george <ga111122222@gmail.com>
2016-01-17 21:07:43 -0800

    got rid of some extraneous ng-blur's used originally for testing. it turns out i can use objectID for my queries (had some trouble with it before)

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var io = require('../io');
 var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
 
 router.get('/api/v1/products/:discriminatorField/:discriminatorValue', (req, res, next) =>
     MongoClient.connect("mongodb://test:test@ds039095.mongolab.com:39095/heroku_vt7zk583", (_, db) => {
@@ -41,8 +42,8 @@ router.post('/api/v1/products', (req, res, next) =>
 router.put('/api/v1/products', (req, res, next) =>
     req.body._id &&
     MongoClient.connect("mongodb://test:test@ds039095.mongolab.com:39095/heroku_vt7zk583", (_, db) => {
-        db.collection('products')
-            .update({[req.body.discriminatorField]: req.body.discriminatorValue}, {$set: {[req.body.changeField]: req.body.newValue}}, {w: 1}, () => {
+            db.collection('products')
+            .update({'_id': ObjectID(req.body._id)}, {$set: {[req.body.changeField]: req.body.newValue}}, {w: 1}, () => {
                 io.emit('update:product', req.body);
                 res.json(req.body);
                 db.close();
